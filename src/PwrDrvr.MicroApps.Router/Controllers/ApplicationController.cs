7d8f92715c25f3a855d1e0933c95d93958659de5
Harold <harold@pwrdrvr.com>
2020-12-17 00:36:03 -0500

    Initiial Router function

--- src/PwrDrvr.MicroApps.Router/Controllers/ApplicationController.cs
+++ src/PwrDrvr.MicroApps.Router/Controllers/ApplicationController.cs
@@ -1,7 +1,7 @@
 using PwrDrvr.MicroApps.DataLib;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using System.Buffers;
+using System.Text;
 
 namespace PwrDrvr.MicroApps.Router.Controllers {
   [ApiController]
@@ -9,7 +9,7 @@ namespace PwrDrvr.MicroApps.Router.Controllers {
   public class ApplicationController : ControllerBase {
     // GET /{appName}
     [HttpGet("{appName}")]
-    async public Task<string> Get(string appName) {
+    async public Task Get(string appName) {
       var versionsAndRules = await Manager.GetVersionsAndRules(appName);
 
       //
@@ -26,9 +26,13 @@ namespace PwrDrvr.MicroApps.Router.Controllers {
 
       var defaultVersion = versionsAndRules.Rules?.RuleSet["default"]?.SemVer;
 
+      // Prepare the iframe contents
+      var semVerUnderscores = defaultVersion.Replace('.', '_');
+      var appVersionPath = string.Format("/{0}/{1}/", appName, semVerUnderscores);
+      var frameHTML = Startup.FrameTemplate.Replace("{{iframeSrc}}", appVersionPath);
 
-
-      return defaultVersion.Replace('.', '_');
+      Response.ContentType = "text/html; charset=UTF-8";
+      await Response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes(frameHTML));
     }
   }
 }
