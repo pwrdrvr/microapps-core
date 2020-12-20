749dc747e2bce84779164c737eafc6a4475f768f
Harold <harold@pwrdrvr.com>
2020-12-19 19:43:39 -0500

    Lambda invoke grant to API Gateway route

--- src/PwrDrvr.MicroApps.Deployer/Lambda/GatewayInfo.cs
+++ src/PwrDrvr.MicroApps.Deployer/Lambda/GatewayInfo.cs
@@ -5,19 +5,21 @@ using Amazon.ApiGatewayV2.Model;
 namespace PwrDrvr.MicroApps.Deployer.Lambda {
   public class GatewayInfo {
     public static async Task<string> GetAPIID(AmazonApiGatewayV2Client apigwy) {
+      return (await GetAPI(apigwy)).ApiId;
+    }
+
+    public static async Task<Api> GetAPI(AmazonApiGatewayV2Client apigwy) {
       var apis = await apigwy.GetApisAsync(new GetApisRequest() {
         MaxResults = "100",
       });
-      string apiId = "";
       // TODO: Handle pagination
       foreach (var api in apis.Items) {
         if (api.Name == "microapps-apis") {
-          apiId = api.ApiId;
-          break;
+          return api;
         }
       }
 
-      return apiId;
+      return null;
     }
 
     public static async Task<string> GetRouterIntegrationId(AmazonApiGatewayV2Client apigwy, string apiId) {
