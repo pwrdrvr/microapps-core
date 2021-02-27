2822239edeb75c7dad8a46a28382143639eb8f3f
Harold <harold@pwrdrvr.com>
2021-02-27 18:43:08 -0500

    Implement Get all Applications

--- src/PwrDrvr.MicroApps.Deployer/Controllers/ApplicationController.cs
+++ src/PwrDrvr.MicroApps.Deployer/Controllers/ApplicationController.cs
@@ -1,9 +1,8 @@
 using PwrDrvr.MicroApps.DataLib;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Amazon.ApiGatewayV2.Model;
-using System;
-using PwrDrvr.MicroApps.Deployer.Lambda;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace PwrDrvr.MicroApps.Deployer.Controllers {
   [ApiController]
@@ -14,6 +13,12 @@ namespace PwrDrvr.MicroApps.Deployer.Controllers {
       public string appName { get; set; }
     }
 
+    // GET /deployer/application
+    [HttpGet()]
+    async public Task<List<DataLib.Models.Application>> Get() {
+      return await Manager.GetAllApps();
+    }
+
     // POST /deployer/application
     [HttpPost()]
     async public Task Post([FromBody] ApplicationBody appBody) {
@@ -22,35 +27,6 @@ namespace PwrDrvr.MicroApps.Deployer.Controllers {
         AppName = appBody.appName,
         DisplayName = appBody.appName,
       });
-
-#if false
-      // Get reference to Router function
-      var lambda = new Amazon.Lambda.AmazonLambdaClient();
-      var function = await lambda.GetFunctionAsync("microapps-router");
-
-      var apigwy = new Amazon.ApiGatewayV2.AmazonApiGatewayV2Client();
-
-      // Get the APIId of the microapps API
-      var apiId = await GatewayInfo.GetAPIID(apigwy);
-
-      // Get integration for Route function
-      string intRouterId = await GatewayInfo.GetRouterIntegrationId(apigwy, apiId);
-
-      var routes = await apigwy.GetRoutesAsync(new GetRoutesRequest() {
-        ApiId = apiId,
-        MaxResults = "100",
-      });
-      foreach (var route in routes.Items) {
-        Console.WriteLine("route: {0}", route.RouteId);
-      }
-
-      // Add Route for new App root, pointing to Router integration
-      var routeRouter = await apigwy.CreateRouteAsync(new CreateRouteRequest() {
-        ApiId = apiId,
-        Target = string.Format("integrations/{0}", intRouterId),
-        RouteKey = string.Format("ANY /{0}", appBody.appName),
-      });
-#endif
 
       // TODO: Update DynamoDB status to indicate integration has been
       // created
