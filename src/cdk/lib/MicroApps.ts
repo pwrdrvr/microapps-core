f9af95e13682af36ec8adb4565fa69968f1e98fe
Harold <harold@pwrdrvr.com>
2021-05-01 23:55:46 -0400

    Fix typo

--- src/cdk/lib/MicroApps.ts
+++ src/cdk/lib/MicroApps.ts
@@ -194,7 +194,7 @@ export class MicroApps extends cdk.Stack {
 
     // TODO: Add Last Route for /*/{proxy+}
     // Note: That might not work, may need a Behavior in CloudFront
-    //       or a Lambda @ Edge function that detecgts these and routes
+    //       or a Lambda @ Edge function that detects these and routes
     //       to origin Lambda Router function.
 
     //
