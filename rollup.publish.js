69399daafda87c689aefd23f1f35708715b2c9d7
Harold <harold@pwrdrvr.com>
2021-06-19 19:38:37 -0400

    Use rollup for publish build... didn't need this

---
+++ rollup.publish.js
@@ -0,0 +1,40 @@
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import externals from 'rollup-plugin-node-externals';
+import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
+
+const LOCAL_EXTERNALS = [];
+const NPM_EXTERNALS = [];
+
+const generateConfig = (input) => ({
+  input: `./src/microapps-publish/src/${input.filename}.ts`,
+  output: {
+    file: `./distb/microapps-publish/${input.filename}${input.minify ? '' : '.max'}.js`,
+    format: 'cjs',
+  },
+  plugins: [
+    json(),
+    commonjs(),
+    externals({}),
+    nodeResolve(),
+    typescript({
+      tsconfig: 'tsconfig.bundle-publish.json',
+    }),
+    input.minify
+      ? terser({
+        compress: true,
+        mangle: true,
+        output: { comments: false }, // Remove all comments, which is fine as the handler code is not distributed.
+      })
+      : undefined,
+  ],
+  external: [...NPM_EXTERNALS, ...LOCAL_EXTERNALS],
+  inlineDynamicImports: true,
+});
+
+export default [
+  { filename: 'index', minify: false },
+  { filename: 'index', minify: true },
+].map(generateConfig);
