4353edb7646ef78779361086b75caa369547220a
Harold <harold@pwrdrvr.com>
2021-06-22 15:45:34 -0400

    Start ephemeral resource deletion

---
+++ src/cdk/lib/SharedProps.ts
@@ -0,0 +1,35 @@
+import { Env } from './Types';
+
+export default class SharedProps {
+  private _env: Env | '';
+  public get env(): Env | '' {
+    if (this._env === '') return '';
+    return this._env;
+  }
+  public get envSuffix(): string {
+    if (this._env === '') return '';
+    return `-${this._env}`;
+  }
+
+  private _pr: string;
+  public get pr(): string {
+    return this._pr;
+  }
+  public get prSuffix(): string {
+    if (this._pr === undefined) return '';
+    return `-${this._pr}`;
+  }
+  public get isPR(): boolean {
+    if (this._pr === undefined) return false;
+    return true;
+  }
+
+  private _stackName = 'microapps';
+  public get stackName(): string {
+    return this._stackName;
+  }
+
+  constructor() {
+    // TODO: Set some values using env vars
+  }
+}
--- src/cdk/lib/SharedTags.ts
+++ src/cdk/lib/SharedTags.ts
@@ -1,13 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import { Env } from './Types';
 
-export default class Tags {
+export default class SharedTags {
   public static addSharedTags(construct: cdk.IConstruct, prSuffix = ''): void {
     cdk.Tags.of(construct).add('Repository', 'https://github.com/pwrdrvr/microapps-core/');
     cdk.Tags.of(construct).add('Application', `microapps-core${prSuffix}`);
   }
 
-  public static addEnvTag(construct: cdk.IConstruct, env: Env, isEphemeral: boolean): void {
+  public static addEnvTag(construct: cdk.IConstruct, env: Env | '', isEphemeral: boolean): void {
     cdk.Tags.of(construct).add('Environment', env);
     if (isEphemeral) {
       cdk.Tags.of(construct).add('Ephemeral', 'true');
