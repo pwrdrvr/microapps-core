e26dd7c54d6f627b3e1ecc469e9003edb33be805
Harold <harold@pwrdrvr.com>
2021-07-02 22:15:51 -0400

    Try to fix supported runtimes

--- src/cdk/lib/MicroAppsSvcs.ts
+++ src/cdk/lib/MicroAppsSvcs.ts
@@ -231,7 +231,6 @@ export class MicroAppsSvcs extends cdk.Construct implements IMicroAppsSvcsExport
     // This is *much* faster on cold inits
     const routerDataFiles = new lambda.LayerVersion(this, 'microapps-router-layer', {
       code: Code.fromAsset('./src/microapps-router/templates/'),
-      compatibleRuntimes: lambda.Runtime.ALL,
     });
     if (shared.isPR) {
       routerDataFiles.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
