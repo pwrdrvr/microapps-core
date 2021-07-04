bfbe46b450800da7e80b2fd28cffe3e775283ad3
Harold <harold@pwrdrvr.com>
2021-07-04 15:33:01 -0400

    Remove fake bucket name

--- src/microapps-deployer/src/controllers/VersionController.ts
+++ src/microapps-deployer/src/controllers/VersionController.ts
@@ -50,7 +50,7 @@ export default class VersionController {
           new iamCDK.PolicyStatement({
             effect: iamCDK.Effect.ALLOW,
             actions: ['s3:PutObject', 's3:GetObject', 's3:AbortMultipartUpload'],
-            resources: [`arn:aws:s3:::${config.filestore.stagingBucket}fake/*`],
+            resources: [`arn:aws:s3:::${config.filestore.stagingBucket}/*`],
           }),
           new iamCDK.PolicyStatement({
             effect: iamCDK.Effect.ALLOW,
