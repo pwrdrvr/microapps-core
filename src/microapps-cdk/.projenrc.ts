fd10bb11be01c7e4ac8149afd1c4da9d83cabe37
Harold <harold@pwrdrvr.com>
2021-07-10 13:25:09 -0400

    Add. CDK Construct starter lib with projen

---
+++ src/microapps-cdk/.projenrc.ts
@@ -0,0 +1,50 @@
+import { AwsCdkConstructLibrary, NodePackageManager, NpmAccess } from 'projen';
+const project = new AwsCdkConstructLibrary({
+  author: 'Harold Hunt',
+  authorAddress: 'harold@pwrdrvr.com',
+  cdkVersion: '1.95.2',
+  copyrightOwner: 'PwrDrvr LLC',
+  copyrightPeriod: '2020',
+  defaultReleaseBranch: 'main',
+  license: 'MIT',
+  name: '@pwrdrvr/microapps-cdk',
+  npmAccess: NpmAccess.PUBLIC,
+  packageManager: NodePackageManager.NPM,
+  projenrcTs: true,
+  repositoryUrl: 'git@github.com:pwrdrvr/microapps-core.git',
+  jest: false,
+
+  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
+  cdkDependencies: [
+    '@aws-cdk/assert',
+    '@aws-cdk/aws-apigatewayv2',
+    '@aws-cdk/aws-apigatewayv2-integrations',
+    '@aws-cdk/aws-certificatemanager',
+    '@aws-cdk/aws-cloudfront',
+    '@aws-cdk/aws-cloudfront-origins',
+    '@aws-cdk/aws-dynamodb',
+    '@aws-cdk/aws-ecr',
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/aws-lambda-nodejs',
+    '@aws-cdk/aws-route53',
+    '@aws-cdk/aws-route53-targets',
+    '@aws-cdk/aws-s3',
+    '@aws-cdk/core',
+  ],
+
+  // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
+  // deps: [],                          /* Runtime dependencies of this module. */
+  deps: [
+    '@cloudcomponents/cdk-container-registry',
+    '@cloudcomponents/cdk-deletable-bucket',
+    '@cloudcomponents/cdk-temp-stack',
+  ],
+
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // release: undefined,                /* Add release management to this project. */
+});
+project.synth();
