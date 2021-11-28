fbce1e805228d77719267f19acd41f130122fcb2
Harold Hunt <harold@pwrdrvr.com>
2021-11-28 11:15:37 -0500

    Fix publish build (#143)

--- packages/microapps-deployer/src/config/APIGateway.ts
+++ packages/microapps-deployer/src/config/APIGateway.ts
@@ -2,24 +2,12 @@ import { Property } from 'ts-convict';
 
 export interface IAPIGateway {
   /**
-   * @deprecated - 2021-11-28 - Use apiId instead
-   */
-  name: string;
-
-  /**
    * ID of the API Gateway to integrate with
    */
   apiId: string;
 }
 
 export class APIGateway implements IAPIGateway {
-  @Property({
-    doc: 'Name of API Gateway to integrate with',
-    default: 'microapps-apis',
-    env: 'APIGWY_NAME',
-  })
-  public name!: string;
-
   @Property({
     doc: 'ID of API Gateway to integrate with',
     nullable: false,
