db25d25ac2f158deb966697d2885b9b7d1713514
Harold Hunt <harold@pwrdrvr.com>
2021-11-27 19:49:50 -0500

    Publish Status Updates (#137)

--- src/microapps-publish/src/S3Uploader.ts
+++ src/microapps-publish/src/S3Uploader.ts
@@ -5,18 +5,50 @@ import { IConfig } from './config/Config';
 import S3TransferUtility from './S3TransferUtility';
 
 export default class S3Uploader {
+  /**
+   * Copy files to local upload directory
+   * @param config
+   * @param s3UploadPath
+   * @param preflightResponse
+   */
+  public static async CopyToUploadDir(config: IConfig, s3UploadPath: string): Promise<void> {
+    const { destinationPrefix } = S3Uploader.ParseUploadPath(s3UploadPath);
+
+    // Make a local root dir for the upload
+    const tempUploadPath = path.join(S3Uploader._tempDir, destinationPrefix);
+    await S3Uploader.removeTempDirIfExists();
+    await fs.mkdir(tempUploadPath, { recursive: true });
+
+    // Copy the files in the source dir to the root dir
+    // Note: It would be faster to move the files, then move them back
+    await fs.copy(config.app.staticAssetsPath, tempUploadPath);
+  }
+
+  public static ParseUploadPath(s3UploadPath: string): {
+    bucketName: string;
+    destinationPrefix: string;
+  } {
+    // Parse the S3 Source URI
+    const uri = new URL(s3UploadPath);
+    const bucketName = uri.host;
+    const destinationPrefix = uri.pathname.length >= 1 ? uri.pathname.slice(1) : '';
+
+    return { bucketName, destinationPrefix };
+  }
+
+  /**
+   * Upload files to S3
+   * @param config
+   * @param s3UploadPath
+   * @param preflightResponse
+   */
   public static async Upload(
     config: IConfig,
     s3UploadPath: string,
     preflightResponse: IDeployVersionPreflightResponse,
   ): Promise<void> {
     try {
-      //const destinationPrefix = `${config.app.name}/${config.app.semVer}`;
-
-      // Parse the S3 Source URI
-      const uri = new URL(s3UploadPath);
-      const bucketName = uri.host;
-      const destinationPrefix = uri.pathname.length >= 1 ? uri.pathname.slice(1) : '';
+      const { destinationPrefix, bucketName } = S3Uploader.ParseUploadPath(s3UploadPath);
 
       // Make a local root dir for the upload
       const tempUploadPath = path.join(S3Uploader._tempDir, destinationPrefix);
@@ -55,4 +87,7 @@ export default class S3Uploader {
   }
 
   private static readonly _tempDir = './deploytool-temp';
+  public static get TempDir(): string {
+    return S3Uploader._tempDir;
+  }
 }
