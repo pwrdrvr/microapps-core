acc1742b8e6d98364365c2d211af45fcf2c701ac
Harold Hunt <harold@pwrdrvr.com>
2022-05-20 10:33:40 -0400

    Fix Broken Prefix (#242)
    Fixes #240

--- .release-replace.config.js
+++ .release-replace.config.js
@@ -1,4 +1,4 @@
 // https://www.npmjs.com/package/replace-in-file
 module.exports = {
-  files: 'node_modules/@pwrdrvr/microapps-app-release-cdk/lib/.serverless_nextjs/**/*,node_modules/@pwrdrvr/microapps-app-release-cdk/lib/.static_files/**/*',
+  files: 'packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/.serverless_nextjs/**/*,packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/.static_files/**/*',
 };
