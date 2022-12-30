59064c26d68357fe6fb26e6ba7ff825096b923c2
Harold <harold@pwrdrvr.com>
2022-12-30 17:41:56 -0500

    Fixup file replace paths

--- .nextjs-demo-replace.config.js
+++ .nextjs-demo-replace.config.js
@@ -1,4 +1,4 @@
 // https://www.npmjs.com/package/replace-in-file
 module.exports = {
-  files: 'packages/cdk/node_modules/@pwrdrvr/microapps-app-nextjs-demo-cdk/lib/.serverless_nextjs/**/*,packages/cdk/node_modules/@pwrdrvr/microapps-app-nextjs-demo-cdk/lib/.static_files/**/*',
+  files: 'packages/cdk/node_modules/@pwrdrvr/microapps-app-nextjs-demo-cdk/lib/microapps-app-nextjs-demo/.next/**/*,packages/cdk/node_modules/@pwrdrvr/microapps-app-nextjs-demo-cdk/lib/microapps-app-nextjs-demo/.serverless_nextjs/**/*,packages/cdk/node_modules/@pwrdrvr/microapps-app-nextjs-demo-cdk/lib/microapps-app-nextjs-demo/.static_files/**/*',
 };
