e7126101c7e8bc5c8a82e64d0f3ddd1eadae60a2
Harold <harold@pwrdrvr.com>
2022-12-31 11:01:49 -0500

    Move replace up a directory on release app

--- .release-replace.config.js
+++ .release-replace.config.js
@@ -1,4 +1,4 @@
 // https://www.npmjs.com/package/replace-in-file
 module.exports = {
-  files: 'packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/.next/**/*,packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/.serverless_nextjs/**/*,packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/.static_files/**/*',
+  files: 'packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/**/*',
 };
