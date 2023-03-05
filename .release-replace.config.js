6b6ce8ed7c183b2c16fe582fc871b3300fab3a30
harold <harold@pwrdrvr.com>
2023-03-05 12:59:10 -0500

    Adjust paths

--- .release-replace.config.js
+++ .release-replace.config.js
@@ -1,4 +1,5 @@
 // https://www.npmjs.com/package/replace-in-file
 module.exports = {
-  files: 'packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/.next/**/*,packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/next.config.js,packages/cdk/node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/.static_files/**/*',
+  files:
+    'node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/.next/**/*,node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/next.config.js,node_modules/@pwrdrvr/microapps-app-release-cdk/lib/microapps-app-release/.static_files/**/*',
 };
