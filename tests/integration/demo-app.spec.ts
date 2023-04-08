f7d9ac34d202a7952f194bae61592d39c9a8b541
harold <harold@pwrdrvr.com>
2023-04-08 19:31:27 -0400

    Fix more tests

--- tests/integration/demo-app.spec.ts
+++ tests/integration/demo-app.spec.ts
@@ -1,5 +1,6 @@
 /// <reference types="jest" />
 import axios from 'axios';
+import posixPath from 'path/posix';
 
 jest.setTimeout(10000);
 jest.retryTimes(2);
@@ -38,7 +39,10 @@ describe('demo-app', () => {
 
     it('should return JSON with an expected value', async () => {
       const response = await axios.get(
-        new URL('/demo-app/serverIncrement?currValue=1', baseUrl).toString(),
+        new URL(
+          posixPath.join(baseUrl.pathname, '/demo-app/serverIncrement?currValue=1'),
+          baseUrl,
+        ).toString(),
         {
           headers: { Accept: 'application/json' },
         },
@@ -58,7 +62,10 @@ describe('demo-app', () => {
   describe('single account - root app', () => {
     it('should return a status of 200', async () => {
       const response = await axios.get(
-        new URL(`/?appver=${DEMO_AND_ROOT_APP_VER}`, baseUrl).toString(),
+        new URL(
+          posixPath.join(baseUrl.pathname, `/?appver=${DEMO_AND_ROOT_APP_VER}`),
+          baseUrl,
+        ).toString(),
         {
           headers: { Accept: 'text/html' },
         },
@@ -71,7 +78,10 @@ describe('demo-app', () => {
 
     it('should return HTML with an expected string', async () => {
       const response = await axios.get(
-        new URL(`/?appver=${DEMO_AND_ROOT_APP_VER}`, baseUrl).toString(),
+        new URL(
+          posixPath.join(baseUrl.pathname, `/?appver=${DEMO_AND_ROOT_APP_VER}`),
+          baseUrl,
+        ).toString(),
         {
           headers: { Accept: 'text/html' },
         },
@@ -86,7 +96,13 @@ describe('demo-app', () => {
 
     it('should return JSON with an expected value', async () => {
       const response = await axios.get(
-        new URL(`/serverIncrement?currValue=1&appver=${DEMO_AND_ROOT_APP_VER}`, baseUrl).toString(),
+        new URL(
+          posixPath.join(
+            baseUrl.pathname,
+            `/serverIncrement?currValue=1&appver=${DEMO_AND_ROOT_APP_VER}`,
+          ),
+          baseUrl,
+        ).toString(),
         {
           headers: { Accept: 'application/json' },
         },
