957430bc446bd477df113ca84c6ffaa50ce77292
harold <harold@pwrdrvr.com>
2023-04-23 10:20:37 -0400

    Locale routing

--- packages/microapps-edge-to-origin/src/index.spec.ts
+++ packages/microapps-edge-to-origin/src/index.spec.ts
@@ -42,248 +42,153 @@ describe('handler', () => {
     process.env.AWS_SESSION_TOKEN = 'session-token';
   });
 
-  it('overwrites host header when enabled', async () => {
-    theConfig.replaceHostHeader = true;
+  describe('host header', () => {
+    const testCases = [
+      { replaceHostHeader: true, expectedHost: 'test-origin.pwrdrvr.com' },
+      { replaceHostHeader: false, expectedHost: 'test.pwrdrvr.com' },
+    ];
 
-    const cfRequest: lambda.CloudFrontRequest = {
-      clientIp: '1.1.1.1',
-      headers: {
-        accept: [
-          {
-            key: 'accept',
-            value:
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-          },
-        ],
-        host: [
-          {
-            key: 'host',
-            value: 'test.pwrdrvr.com',
-          },
-        ],
-        'x-forwarded-for': [
-          {
-            key: 'x-forwarded-for',
-            value: '1.1.1.1, 2.2.2.2',
-          },
-        ],
-      },
-      method: 'GET',
-      querystring: 'param1=value1&param2=value2',
-      uri: '/test/test.html',
-      origin: {
-        // @ts-expect-error not specifying full obj
-        custom: {
-          domainName: 'test-origin.pwrdrvr.com',
-        },
-      },
-    };
+    it.each(testCases)(
+      'host header when replaceHostHeader is %p',
+      async ({ replaceHostHeader, expectedHost }) => {
+        theConfig.replaceHostHeader = replaceHostHeader;
 
-    const signedRequest = await handler(
-      {
-        Records: [
-          {
-            cf: {
-              request: cfRequest,
-              config: {
-                distributionDomainName: 'test-edge.pwrdrvr.com',
-                distributionId: 'ABC123',
-                eventType: 'origin-request',
-                requestId: 'ABC456',
+        const cfRequest: lambda.CloudFrontRequest = {
+          clientIp: '1.1.1.1',
+          headers: {
+            accept: [
+              {
+                key: 'accept',
+                value:
+                  'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
               },
+            ],
+            host: [
+              {
+                key: 'host',
+                value: 'test.pwrdrvr.com',
+              },
+            ],
+            'x-forwarded-for': [
+              {
+                key: 'x-forwarded-for',
+                value: '1.1.1.1, 2.2.2.2',
+              },
+            ],
+          },
+          method: 'GET',
+          querystring: 'param1=value1&param2=value2',
+          uri: '/test/test.html',
+          origin: {
+            // @ts-expect-error not specifying full obj
+            custom: {
+              domainName: 'test-origin.pwrdrvr.com',
             },
           },
-        ],
-      },
-      {} as lambda.Context,
-      () => undefined,
-    );
+        };
 
-    expect(signedRequest).toBeDefined();
-    expect(signedRequest!.headers).toBeDefined();
-    expect(signedRequest!.headers!['host'][0].value).toEqual('test-origin.pwrdrvr.com');
+        const signedRequest = await handler(
+          {
+            Records: [
+              {
+                cf: {
+                  request: cfRequest,
+                  config: {
+                    distributionDomainName: 'test-edge.pwrdrvr.com',
+                    distributionId: 'ABC123',
+                    eventType: 'origin-request',
+                    requestId: 'ABC456',
+                  },
+                },
+              },
+            ],
+          },
+          {} as lambda.Context,
+          () => undefined,
+        );
+
+        expect(signedRequest).toBeDefined();
+        expect(signedRequest!.headers).toBeDefined();
+
+        expect(signedRequest!.headers!['host'][0].value).toEqual(expectedHost);
+      },
+    );
   });
 
-  it('does not overwrite host header when disabled', async () => {
-    theConfig.replaceHostHeader = false;
+  describe('x-forwarded-host header', () => {
+    const testCases = [
+      { addXForwardedHostHeader: true, expectedHost: 'test.pwrdrvr.com' },
+      { addXForwardedHostHeader: false, expectedHost: undefined },
+    ];
 
-    const cfRequest: lambda.CloudFrontRequest = {
-      clientIp: '1.1.1.1',
-      headers: {
-        accept: [
-          {
-            key: 'accept',
-            value:
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-          },
-        ],
-        host: [
-          {
-            key: 'host',
-            value: 'test.pwrdrvr.com',
-          },
-        ],
-        'x-forwarded-for': [
-          {
-            key: 'x-forwarded-for',
-            value: '1.1.1.1, 2.2.2.2',
-          },
-        ],
-      },
-      method: 'GET',
-      querystring: 'param1=value1&param2=value2',
-      uri: '/test/test.html',
-      origin: {
-        // @ts-expect-error not specifying full obj
-        custom: {
-          domainName: 'test-origin.pwrdrvr.com',
-        },
-      },
-    };
+    it.each(testCases)(
+      'host header when replaceHostHeader is %p',
+      async ({ addXForwardedHostHeader, expectedHost }) => {
+        theConfig.addXForwardedHostHeader = addXForwardedHostHeader;
 
-    const signedRequest = await handler(
-      {
-        Records: [
-          {
-            cf: {
-              request: cfRequest,
-              config: {
-                distributionDomainName: 'test-edge.pwrdrvr.com',
-                distributionId: 'ABC123',
-                eventType: 'origin-request',
-                requestId: 'ABC456',
+        const cfRequest: lambda.CloudFrontRequest = {
+          clientIp: '1.1.1.1',
+          headers: {
+            accept: [
+              {
+                key: 'accept',
+                value:
+                  'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
               },
+            ],
+            host: [
+              {
+                key: 'host',
+                value: 'test.pwrdrvr.com',
+              },
+            ],
+            'x-forwarded-for': [
+              {
+                key: 'x-forwarded-for',
+                value: '1.1.1.1, 2.2.2.2',
+              },
+            ],
+          },
+          method: 'GET',
+          querystring: 'param1=value1&param2=value2',
+          uri: '/test/test.html',
+          origin: {
+            // @ts-expect-error not specifying full obj
+            custom: {
+              domainName: 'test-origin.pwrdrvr.com',
             },
           },
-        ],
-      },
-      {} as lambda.Context,
-      () => undefined,
-    );
+        };
 
-    expect(signedRequest).toBeDefined();
-    expect(signedRequest!.headers).toBeDefined();
-    expect(signedRequest!.headers!['host'][0].value).toEqual('test.pwrdrvr.com');
-  });
-
-  it('adds x-forwarded-host when enabled', async () => {
-    theConfig.addXForwardedHostHeader = true;
-
-    const cfRequest: lambda.CloudFrontRequest = {
-      clientIp: '1.1.1.1',
-      headers: {
-        accept: [
+        const signedRequest = await handler(
           {
-            key: 'accept',
-            value:
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-          },
-        ],
-        host: [
-          {
-            key: 'host',
-            value: 'test.pwrdrvr.com',
-          },
-        ],
-        'x-forwarded-for': [
-          {
-            key: 'x-forwarded-for',
-            value: '1.1.1.1, 2.2.2.2',
-          },
-        ],
-      },
-      method: 'GET',
-      origin: {
-        // @ts-expect-error do not need full object for this test
-        custom: { domainName: 'random-but-account-specific-name.lambda-url.us-east-2.on.aws' },
-      },
-      querystring: 'param1=value1&param2=value2',
-      uri: '/test/test.html',
-    };
-
-    const signedRequest = await handler(
-      {
-        Records: [
-          {
-            cf: {
-              request: cfRequest,
-              config: {
-                distributionDomainName: 'test-edge.pwrdrvr.com',
-                distributionId: 'ABC123',
-                eventType: 'origin-request',
-                requestId: 'ABC456',
+            Records: [
+              {
+                cf: {
+                  request: cfRequest,
+                  config: {
+                    distributionDomainName: 'test-edge.pwrdrvr.com',
+                    distributionId: 'ABC123',
+                    eventType: 'origin-request',
+                    requestId: 'ABC456',
+                  },
+                },
               },
-            },
+            ],
           },
-        ],
+          {} as lambda.Context,
+          () => undefined,
+        );
+
+        expect(signedRequest).toBeDefined();
+        expect(signedRequest!.headers).toBeDefined();
+        if (addXForwardedHostHeader) {
+          expect(signedRequest!.headers!['x-forwarded-host'][0].value).toEqual(expectedHost);
+        } else {
+          expect(signedRequest!.headers!['x-forwarded-host']).toBeUndefined();
+        }
       },
-      {} as lambda.Context,
-      () => undefined,
     );
-
-    expect(signedRequest).toBeDefined();
-    expect(signedRequest!.headers).toBeDefined();
-    expect(signedRequest!.headers!['x-forwarded-host'][0].value).toEqual('test.pwrdrvr.com');
-  });
-
-  it('does not add x-forwarded-host when disabled', async () => {
-    theConfig.addXForwardedHostHeader = false;
-
-    const cfRequest: lambda.CloudFrontRequest = {
-      clientIp: '1.1.1.1',
-      headers: {
-        accept: [
-          {
-            key: 'accept',
-            value:
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-          },
-        ],
-        host: [
-          {
-            key: 'host',
-            value: 'test.pwrdrvr.com',
-          },
-        ],
-        'x-forwarded-for': [
-          {
-            key: 'x-forwarded-for',
-            value: '1.1.1.1, 2.2.2.2',
-          },
-        ],
-      },
-      method: 'GET',
-      querystring: 'param1=value1&param2=value2',
-      uri: '/test/test.html',
-      origin: {
-        // @ts-expect-error do not need full object for this test
-        custom: { domainName: 'random-but-account-specific-name.lambda-url.us-east-2.on.aws' },
-      },
-    };
-
-    const signedRequest = await handler(
-      {
-        Records: [
-          {
-            cf: {
-              request: cfRequest,
-              config: {
-                distributionDomainName: 'test-edge.pwrdrvr.com',
-                distributionId: 'ABC123',
-                eventType: 'origin-request',
-                requestId: 'ABC456',
-              },
-            },
-          },
-        ],
-      },
-      {} as lambda.Context,
-      () => undefined,
-    );
-
-    expect(signedRequest).toBeDefined();
-    expect(signedRequest!.headers).toBeDefined();
-    expect(signedRequest!.headers!['x-forwarded-host']).toBeUndefined();
   });
 
   it('does not sign request when disabled', async () => {
