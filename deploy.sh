0a3f4da10ab9fc6de7f5a31c889a33452d5b3add
harold <harold@pwrdrvr.com>
2023-05-19 00:01:30 -0400

    Remove erroneous --no-paginate

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,13 @@ npx cdk deploy microapps-core --context @pwrdrvr/microapps:deployReleaseApp=true
 RELEASE_APP_PACKAGE_VERSION=$(node -p -e "require('./node_modules/@pwrdrvr/microapps-app-release-cdk/package.json').version")
 
 # Extract Deployer Lambda Name
-DEPLOYER_LAMBDA_NAME="$(aws cloudformation list-exports --query "Exports[?Name==\`microapps-demo-deploy-${NODE_ENV}-deployer-func-name\`].Value" --no-paginate --output text)"
+DEPLOYER_LAMBDA_NAME="$(aws cloudformation list-exports --query "Exports[?Name==\`microapps-demo-deploy-${NODE_ENV}-deployer-func-name\`].Value"  --output text)"
 
 # Extract Release App Lambda Name
-RELEASE_APP_LAMBDA_NAME=$(aws cloudformation list-exports --query "Exports[?Name==\`microapps-demo-deploy-${NODE_ENV}-release-app-func-name\`].Value" --no-paginate --output text)
+RELEASE_APP_LAMBDA_NAME=$(aws cloudformation list-exports --query "Exports[?Name==\`microapps-demo-deploy-${NODE_ENV}-release-app-func-name\`].Value"  --output text)
 
 # Extract Edge Domain Name
-EDGE_DOMAIN=$(aws cloudformation list-exports --query "Exports[?Name==\`microapps-demo-deploy-${NODE_ENV}-edge-domain-name\`].Value" --no-paginate --output text)
+EDGE_DOMAIN=$(aws cloudformation list-exports --query "Exports[?Name==\`microapps-demo-deploy-${NODE_ENV}-edge-domain-name\`].Value"  --output text)
 
 # Deploy the Release App
 npx microapps-publish publish --app-name release --new-version ${RELEASE_APP_PACKAGE_VERSION} --deployer-lambda-name ${DEPLOYER_LAMBDA_NAME} --app-lambda-name ${RELEASE_APP_LAMBDA_NAME} --static-assets-path node_modules/@pwrdrvr/microapps-app-release-cdk/lib/static_files/release/${RELEASE_APP_PACKAGE_VERSION}/ --overwrite --no-cache
