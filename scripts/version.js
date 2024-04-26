2e2c9d2b0edbb9a8b28683652791cbb4cb466729
Harold Hunt <harold@pwrdrvr.com>
2024-04-25 23:54:22 -0400

    Cleanup version in the script

--- scripts/version.js
+++ scripts/version.js
@@ -1,12 +1,21 @@
 const glob = require('glob');
 const fs = require('fs');
 const path = require('path');
+const semver = require('semver');
 
 // Get the new version from the command-line arguments
-const newVersion = process.argv[2];
+let newVersion = process.argv[2];
+
+if (!newVersion) {
+  console.error('Please provide a version as the first argument.');
+  console.log(`Usage: node ${path.relative(process.cwd(), process.argv[1])} x.y.z`);
+  process.exit(1);
+}
+
+newVersion = newVersion.replace(/.*\//, '').replace(/^v/, '');
 
 // Validate the new version
-if (!newVersion || !/^\d+\.\d+\.\d+$/.test(newVersion)) {
+if (!semver.valid(newVersion)) {
   console.error(
     'Invalid version. Please provide a version in the format x.y.z as the first argument.',
   );
